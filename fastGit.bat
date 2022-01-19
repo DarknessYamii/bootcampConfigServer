bc182de76552612bf842f3070f13f933c60ef997
Andrés Ruiz Delgado <andresruizdelgado12@gmail.com>
2022-01-19 17:05:51 +0100

    first commit

---
+++ fastGit.bat
@@ -0,0 +1,7 @@
+echo "# bootcampConfigServer" >> README.md
+git init
+git add -A
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/DarknessYamii/bootcampConfigServer.git
+git push -u origin main
